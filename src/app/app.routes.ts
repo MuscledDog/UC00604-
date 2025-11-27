944b019d4d35260b1af2f43b1ffdd5b7d284c27a
MuscledDog <lemos.vasco@gmail.com>
2025-11-27 21:57:36 +0000

    adcionado paginas autenticacao/uc6004

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { Uc00604 } from './pages/uc00604/uc00604';
+import { Authentication } from './pages/authentication/authentication';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  /* Nossa 1ª rota */
+    {path:'', redirectTo:"welcome", pathMatch:'full'},
+
+    {path:"welcome", loadComponent: () => import('./pages/welcome/welcome') .then ((vasco) => vasco.Welcome)} ,
+
+    {path:"pages/autenticacao", loadComponent: () => import("./pages/authentication/authentication") .then (ze => ze.Authentication)},
+
+  /** Rota padrão sem lazyload */
+
+    {path:"pages/uc00604", component: Uc00604},
+
+
+    /** Nossa ultima rota */
+    {path:'**', loadComponent:() => import('./pages/welcome/welcome') .then(c => c.Welcome)},
+];
